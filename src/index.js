4cbcd30d5c5e1a7381fddd9ce12e4a9fed10ffa6
wolkeaogpush <71719698+wolkeaogpush@users.noreply.github.com>
2020-09-22 23:59:48 +0800

    add pr

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const server = express();
 const utils = require("./utils");
 server
   .get("/", (req, rep) => {
-    rep.end(process.env.key + "test");
+    rep.end(process.env.key + " pr");
   })
   .listen(8080, () => console.log("running"));
